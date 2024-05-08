6e39a870b728e8d7a1cbd8c0b06981ac7dcb7d81
mpan0326 <168643194+mpan0326@users.noreply.github.com>
2024-05-08 08:44:33 -0400

    Add files via upload

---
+++ bird.py
@@ -0,0 +1,28 @@
+import pygame
+import random
+
+
+class Bird:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("bird.png")
+        self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = 3
+
+    def rescale_image(self, image):
+        self.image_size = self.image.get_size()
+        scale_size = (self.image_size[0] * .7, self.image_size[1] * .7)
+        self.image = pygame.transform.scale(self.image, scale_size)
+
+    def move_bird(self, direction):
+        if direction == "UP":
+            if self.y >= 10:
+                self.y = int(self.y) - self.delta
+        if direction == "DOWN":
+            if self.y <= 400:
+                self.y = int(self.y) + self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import pygame
 import random
-from playsound import playsound
-from birdie import Bird
+#from playsound import playsound
+from bird import Bird
 
 # set up pygame modules
 pygame.init()
 pygame.font.init()
 my_font = pygame.font.SysFont('Arial', 20)
+title_font = pygame.font.SysFont('Arial', 40)
 pygame.display.set_caption("Flappy Crow")
+title_mg = my_font
 
 
 # set up variables for the display
-size = (800, 600)
+size = (800, 450)
 screen = pygame.display.set_mode(size)
-BIRD_START_X = 500
+bird_x = 300
+bird_y = 200
 
 bg = pygame.image.load("background.png")
-pipe1 = pygame.image.load()
-telephone_pole = pygame.image.load()
+#pipe1 = pygame.image.load()
+#telephone_pole = pygame.image.load()
 
-b = Bird(300, 200)
+b = Bird(bird_x, bird_y)
 
 
 INITIAL_HOUSE_X = random.randint(0,600)
@@ -48,9 +51,8 @@ while run:
             run = False
 
     screen.blit(bg, (0, 0))
-    screen.blit(house, (house_x, 360))
-    screen.blit(tree, (tree_x, 360))
-    screen.blit(bird.image, bird.rect)
+    #screen.blit(house, (house_x, 360))
+    #screen.blit(tree, (tree_x, 360))
     screen.blit(b.image, b.rect)
     pygame.display.update()
 
