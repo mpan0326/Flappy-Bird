20bd59774dcebfdc7c17882ffa2d11f29455faca
mpan0326 <168643194+mpan0326@users.noreply.github.com>
2024-05-17 08:41:29 -0400

    Add files via upload

--- cloud.py
+++ cloud.py
@@ -1,16 +1,16 @@
 import pygame
 
 
-class Fish:
+class Cloud:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("fish.png")
+        self.image = pygame.image.load("cloud.png")
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-        self.delta = 1
+        self.delta = 3
 
-    def move_fish(self):
-        self.x = self.x + 2
+    def move_cat(self):
+        self.x = self.x + 1.5
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 #from playsound import playsound
 from bird import Bird
-from cat import Cat
+from cloud import Cloud
 from fish import Fish
 
 # set up pygame modules
@@ -13,7 +13,8 @@ title_font = pygame.font.SysFont('Arial', 40)
 pygame.display.set_caption("Flappy Goose")
 title_mg = title_font.render("FLAPPY GOOSE", True, (61, 76, 125))
 howto_st = my_font.render("Click to begin", True, (61, 76, 125))
-howto_move = my_font.render("Press space to move up and down to dodge the cat", True, (61, 76, 125))
+howto_move = my_font.render("Press space to move up and down to dodge the cloud", True, (61, 76, 125))
+score_show = my_font.render("Score: 0", True, (61, 76, 125))
 
 
 
@@ -22,21 +23,22 @@ size = (800, 450)
 screen = pygame.display.set_mode(size)
 bird_x = 370
 bird_y = 200
-cat_x = 20
-cat_y = 200
 fish_x = 10
 fish_y = 300
-cloud_x = 150
+cloud_x = 40
+cloud_y = 200
 
 
 
 bg = pygame.image.load("background.png")
-cloud = pygame.image.load("cloud.png")
 b = Bird(bird_x, bird_y)
-c = Cat(cat_x, cat_y)
+c = Cloud(cloud_x, cloud_y)
+f = Fish(fish_x, fish_y)
 
 st_msg_show = True
 score = 0
+fish_hit = False
+cloud_hit = False
 
 
 # render the text for later
@@ -61,27 +63,36 @@ while run:
             st_msg_show = False
 
     c.move_cat()
-    cloud_x += 1
+    f.move_fish()
 
-    if cloud_x >= 900:
-        cloud_x = -150
+    if f.x >= 870 or fish_hit == True:
+        f.x = -70
+        f.y = random.randint(70, 390)
 
     if c.x >= 890:
         c.x = -140
         c.y = random.randint(125, 325)
+
+    if b.rect.colliderect(c.rect):
+        cloud_hit = True
+    if b.rect.colliderect(f.rect):
+        fish_hit = True
+
+#make score thing etc jfwhIFw
 
 
 
     screen.blit(bg, (0, 0))
     if st_msg_show == True:
         screen.blit(title_mg, (230, 160))
-        screen.blit(howto_move, (160, 210))
+        screen.blit(howto_move, (155, 210))
         screen.blit(howto_st, (320, 240))
 
     if st_msg_show == False:
         screen.blit(b.image, b.rect)
         screen.blit(c.image, c.rect)
-        screen.blit(cloud, (cloud_x, 100))
+        screen.blit(f.image, f.rect)
+        screen.blit(show_score, (0,0))
     pygame.display.update()
 
     frame += 1
